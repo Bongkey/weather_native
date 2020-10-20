d9c991303cd6293a82144aa27af9528ef7c6870f
Ki.BongJo <Kibongjo1017@gmail.com>
2020-10-20 14:56:33 +0900

    # Eslint & prettier 적용

---
+++ Loading.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { StyleSheet, Text, View } from 'react-native';
+
+function Loading() {
+    return <View></View>
+}
+
+
+const styles = StyleSheet.create({
+
+})
